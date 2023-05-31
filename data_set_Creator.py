7e241dec382a097844a1b56cfcc620705c727caa
Shaique <solankishaique@gmail.com>
2023-05-31 19:31:40 +0530

    Data set creator of all controls

---
+++ data_set_Creator.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 31 17:33:19 2023
+
+@author: Spirelab
+"""
+
+import pandas as pd
+import numpy
+import os
+import shutil
+ 
+control_dir = "C:/Users/Spirelab/Desktop/mfcc_correlation/mfcc_data_set/controls/controls"
+
+control_mfcc_dir = "C:/Users/Spirelab/Desktop/mfcc_correlation/mfcc_data_set/control_mfcc"
+
+os.chdir(control_dir)
+
+subjects = os.listdir(control_dir)
+
+for i in subjects:
+    
+    subject_dir = os.path.join(control_dir, i)
+        
+    mfcc_folder_names = os.path.join(control_mfcc_dir, i)
+        
+    mfcc_dirs = os.makedirs(mfcc_folder_names)
+    
+    sub_dir = os.path.join(control_mfcc_dir, i)
+    
+    csv_files = [f for f in os.listdir(subject_dir) if f.endswith(".csv")]
+    
+    for j in csv_files:
+        
+        csv_file_dir = os.path.join(subject_dir, j)
+        
+        shutil.copy(csv_file_dir, sub_dir)
+    
+    
